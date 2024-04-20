d1642139165980a16b81b2237c69627d6863e3d4
Walisson Silva <walissonsilva.dev@gmail.com>
2024-04-20 13:57:05 -0300

    feat(plugin): basic data extraction plugin

---
+++ plugin/src/data-extraction-plugin.ts
@@ -0,0 +1,33 @@
+type ExtractedData = {
+  device: string;
+  os: string;
+  origin: string;
+};
+
+export function extractData(): ExtractedData {
+  const userAgent = navigator.userAgent;
+  const platform = navigator.platform;
+  const origin = window.location.hostname;
+
+  let device = "desktop";
+  if (/android/i.test(userAgent)) {
+    device = "android";
+  } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MediaStream) {
+    device = "ios";
+  }
+
+  const os = platform;
+
+  return { device, os, origin };
+}
+
+function activatePlugin() {
+  console.log("🔶 Data Extraction Plugin is activated! 🚀");
+
+  const { device, os, origin } = extractData();
+  console.log("Device:", device);
+  console.log("OS:", os);
+  console.log("Origin:", origin);
+}
+
+activatePlugin();
